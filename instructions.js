f025b991524de7738ba078e40d3a7a81b9614229
StitchNG <stitchnig@gmail.com>
2022-03-29 15:48:40 +0100

    Create instructions.js

---
+++ instructions.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/**
+ * adonis-sse
+ *
+ * @license MIT
+ * @copyright Slynova - Romain Lanz <romain.lanz@slynova.ch>
+ *
+ * @extended Oparand - Ifeora Okechukwu <isocroft@gmail.com>
+ */
+
+const path = require('path')
+
+module.exports = async function (cli) {
+  try {
+    await cli.makeConfig('verifyme.js', path.join(__dirname, './config/verifyme.js'))
+    cli.command.completed('create', 'config/verifyme.js')
+  } catch (error) {
+    // ignore if verifyme.js file already exists
+  }
+}
